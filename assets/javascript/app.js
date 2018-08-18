aef01bfdb3641ee3ba28d0ed8672199d30dd2c38
Timmy <123.tsa@gmail.com>
2018-08-18 01:28:03 -0400

    12th

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,19 @@
-//global variables
 // $("#start").on('click', function(){
 //     console.log("OUCH!")
 //     alert("Stop hitting me!!")
 // });
 
 $("#start").on('click', function(){
-    $("#quizBlock").remove();
-    for(var i = 0; i < questions.length; i++){
-        $("#quizBlock").append('<h2>'+ questions[i].question+'</h2>');
-        for(var s = 0; s < questions[i].solution.length; s++){
-            $("#quizBlock").append("<input type='radio' name='question-" + i + "'value='" + questions[i].solution[s] + "'>" + questions[i].solution[s]);
-        }
-    }
+    game.start();
+    // $("#quizBlock").remove();
+
+    // for(var i = 0; i < questions.length; i++){
+    //     $("#quizBlock").append('<h2>'+ questions[i].question+'</h2>');
+
+    //     for(var s = 0; s < questions[i].solution.length; s++){
+    //         $("#quizBlock").append("<input type='radio' name='question-" + i + "'value='" + questions[i].solution[s] + "'>" + questions[i].solution[s]);
+    //     }
+    // }
 });
 
 // $(document).on('click', "#finish", function(){
@@ -21,8 +23,6 @@ $("#start").on('click', function(){
 // var timer;
 
 // var qn = $("#quizBlock");
-
-// ****TRY TO SEPARATE THE QN, CHOICES, AND SOLN ARRAYS!!******
 
 var questions = [{
     question: "What was the first full length CGI movie?",
@@ -45,7 +45,7 @@ var game = {
     countdown: function(){
         game.counter--;
         $("#decrement").html(game.counter);
-        if(game.counter === 0){
+        if(game.counter <= 0){
             console.log("Game over!");
             game.end();
         }
@@ -55,18 +55,18 @@ var game = {
 
         timer = setInterval(game.countdown, 1000);
 
-        $("#sub-wrapper").prepend("<h2>Beat the clock... <span id='decrement'>10</span></h2>");
+        $("#miniWrapper").prepend("<h2>Beat the clock... <span id='decrement'>10</span></h2>");
 
         $("#start").remove();
 
         for(var q = 0; q < questions.length; q++){
-            qn.append("<h2>" + questions[q].question + "</h2>");
+            $("#miniWrapper").append("<h2>" + questions[q].question + "</h2>");
             for(var a = 0; a < questions[q].choices.length; a++){
-                qn.append("<input type='radio' name='question-" + q + "' value='" + questions[q].choices[a] + "''>" + questions[q].choices[a]);
+                $("#miniWrapper").append("<input type='radio' name='question-" + q + "' value='" + questions[q].choices[a] + "''>" + questions[q].choices[a]);
             }
         }
 
-        qn.append("<button id='finish'>I'm So Done!</button>");
+        $("#miniWrapper").append("</br></br><button id='finish'>I'm So Done!</button>");
     },
 
     end: function(){
@@ -196,22 +196,18 @@ var game = {
     },
 
     result: function(){
-
+        //stop the timer 
         clearInterval(timer);
 
-        $("#sub-wrapper h2").remove();
+        $("#miniWrapper h2").remove();
 
-        qn.html("<h2>Finito!!");
-        qn.append("<h3>You answered: " + this.right + " correctly.</h3>")
-        qn.append("<h3>You missed: " + this.wrong + " questions.</h3>")
-        qn.append("<h3>You skipped: " + (questions.length - (this.right + this.wrong)) + " questions.</h3>");
+        $("#miniWrapper").html("<h2>Finito!!</h2>");
+        $("#miniWrapper").append("<h3>You answered: " + this.right + " correctly.</h3>")
+        $("#miniWrapper").append("<h3>You missed: " + this.wrong + " questions.</h3>")
+        $("#miniWrapper").append("<h3>You skipped: " + (questions.length - (this.right + this.wrong)) + " questions.</h3>");
     }
 
 };
-
-$("#start").on('click', function(){
-    game.start();
-});
 
 // $(document).on('click', "#start", function(){
 //     game.start();
