258ef7c88c81d1534ea77559dddeccee95310c21
Timmy <123.tsa@gmail.com>
2018-08-17 21:57:23 -0400

    11

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,15 +6,23 @@
 
 $("#start").on('click', function(){
     $("#quizBlock").remove();
-})
+    for(var i = 0; i < questions.length; i++){
+        $("#quizBlock").append('<h2>'+ questions[i].question+'</h2>');
+        for(var s = 0; s < questions[i].solution.length; s++){
+            $("#quizBlock").append("<input type='radio' name='question-" + i + "'value='" + questions[i].solution[s] + "'>" + questions[i].solution[s]);
+        }
+    }
+});
 
-// $(document).on('click', "#done", function(){
-//     game.done();
+// $(document).on('click', "#finish", function(){
+//     game.end();
 // });
 
 // var timer;
 
 // var qn = $("#quizBlock");
+
+// ****TRY TO SEPARATE THE QN, CHOICES, AND SOLN ARRAYS!!******
 
 var questions = [{
     question: "What was the first full length CGI movie?",
@@ -30,23 +38,24 @@ var questions = [{
 }];
 
 var game = {
-    counter: 120,
     right: 0,
     wrong: 0,
+    counter: 10,
 
     countdown: function(){
         game.counter--;
-        $("#counter-number").html(game.counter);
+        $("#decrement").html(game.counter);
         if(game.counter === 0){
-            console.log("Time is up!");
-            game.done();
+            console.log("Game over!");
+            game.end();
         }
     },
 
     start: function(){
+
         timer = setInterval(game.countdown, 1000);
 
-        $("#sub-wrapper").prepend("<h2>Time Remaining: <span id='counter-number'>10</span> Seconds</h2>");
+        $("#sub-wrapper").prepend("<h2>Beat the clock... <span id='decrement'>10</span></h2>");
 
         $("#start").remove();
 
@@ -57,13 +66,125 @@ var game = {
             }
         }
 
-        qn.append("<button id='done'>I'm So Done!</button>");
+        qn.append("<button id='finish'>I'm So Done!</button>");
     },
 
-    done: function(){
-
+    end: function(){
+        //checks if user chose the correct answer
         $.each($("input[name='question-0']:checked"), function(){
             if($(this).val() === questions[0].right){
+                game.right++;
+            } else{
+                game.wrong++;
+            }
+        });
+
+        $.each($("input[name='question-0']:checked"), function(){
+            if($(this).val() === questions[1].right){
+                game.right++;
+            } else{
+                game.wrong++;
+            }
+        });
+
+        $.each($("input[name='question-0']:checked"), function(){
+            if($(this).val() === questions[2].right){
+                game.right++;
+            } else{
+                game.wrong++;
+            }
+        });
+
+        $.each($("input[name='question-0']:checked"), function(){
+            if($(this).val() === questions[3].right){
+                game.right++;
+            } else{
+                game.wrong++;
+            }
+        });
+
+        $.each($("input[name='question-0']:checked"), function(){
+            if($(this).val() === questions[4].right){
+                game.right++;
+            } else{
+                game.wrong++;
+            }
+        });
+
+        $.each($("input[name='question-0']:checked"), function(){
+            if($(this).val() === questions[5].right){
+                game.right++;
+            } else{
+                game.wrong++;
+            }
+        });
+
+        $.each($("input[name='question-0']:checked"), function(){
+            if($(this).val() === questions[6].right){
+                game.right++;
+            } else{
+                game.wrong++;
+            }
+        });
+
+        $.each($("input[name='question-0']:checked"), function(){
+            if($(this).val() === questions[7].right){
+                game.right++;
+            } else{
+                game.wrong++;
+            }
+        });
+
+        $.each($("input[name='question-0']:checked"), function(){
+            if($(this).val() === questions[8].right){
+                game.right++;
+            } else{
+                game.wrong++;
+            }
+        });
+
+        $.each($("input[name='question-0']:checked"), function(){
+            if($(this).val() === questions[9].right){
+                game.right++;
+            } else{
+                game.wrong++;
+            }
+        });
+
+        $.each($("input[name='question-0']:checked"), function(){
+            if($(this).val() === questions[10].right){
+                game.right++;
+            } else{
+                game.wrong++;
+            }
+        });
+
+        $.each($("input[name='question-0']:checked"), function(){
+            if($(this).val() === questions[11].right){
+                game.right++;
+            } else{
+                game.wrong++;
+            }
+        });
+
+        $.each($("input[name='question-0']:checked"), function(){
+            if($(this).val() === questions[12].right){
+                game.right++;
+            } else{
+                game.wrong++;
+            }
+        });
+
+        $.each($("input[name='question-0']:checked"), function(){
+            if($(this).val() === questions[13].right){
+                game.right++;
+            } else{
+                game.wrong++;
+            }
+        });
+
+        $.each($("input[name='question-0']:checked"), function(){
+            if($(this).val() === questions[14].right){
                 game.right++;
             } else{
                 game.wrong++;
@@ -96,10 +217,10 @@ $("#start").on('click', function(){
 //     game.start();
 // });
 
-$("#done").on('click', function(){
-    game.done();
+$("#finish").on('click', function(){
+    game.end();
 });
 
-// $(document).on('click', "#done", function(){
-//     game.done();
+// $(document).on('click', "#finish", function(){
+//     game.end();
 // });
